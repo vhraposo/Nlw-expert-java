6545d9addae403fd314ef0fa4056c8c0bd0b2cac
vhraposo <thevhup@gmail.com>
2024-01-31 20:43:18 -0300

    Aula 03

--- src/main/java/com/rocketseat/certification_nlw/modules/students/repositories/CertificationStudentRepository.java
+++ src/main/java/com/rocketseat/certification_nlw/modules/students/repositories/CertificationStudentRepository.java
@@ -13,4 +13,7 @@ import com.rocketseat.certification_nlw.modules.students.entities.CertificationS
 public interface CertificationStudentRepository extends JpaRepository<CertificationStudentEntity, UUID>{
     @Query("SELECT c FROM certifications c INNER JOIN c.studentEntity std WHERE std.email = :email AND c.technology = :technology")
     List<CertificationStudentEntity> findByStudentEmailAndTechnology(String email, String technology);
+
+    @Query("SELECT c from certifications c ORDER BY c.grade DESC LIMIT 10")
+    List<CertificationStudentEntity> findTop10ByOrderByGradeDesc();
 }
